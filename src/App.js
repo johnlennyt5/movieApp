5be93e5604b2a5aab111f71fa45429f00ae1f887
johnlennyt5 <johnlennyt@gmail.com>
2023-05-26 11:37:22 -0400

    added movie app to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+import searchIcon from './search.svg'
+
+
+const API_URL = "http://www.omdbapi.com?apikey=817bca82";
+
+const movie1 = {
+    "Title": "The Amazing Spiderman 2 Webb Cut",
+    "Year": "2021",
+    "imdbID": "tt18351128",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BYzYzZDViNWYtNWViMS00NDMxLThlN2YtZjFkOWMwODkzNzhiXkEyXkFqcGdeQXVyMTUwMzM4NzU0._V1_SX300.jpg"
 }
 
-export default App;
+const App = () => {
+
+    const seacrMovies = async (title) => {
+        const response = await fetch (`${API_URL}&s=${title}`)
+        const data = await response.json()
+
+
+        console.log(data.Search);
+    }
+useEffect(() => {
+    seacrMovies('Spiderman')
+}, [])
+
+    return(
+       <div className='app'>
+        <h1>MovieWorld</h1>
+
+        <div className='secah'>
+            <input placeholder=' Search for movies' value='SuperMan' 
+            onChange={() => {}} 
+            />
+            <img 
+            src={searchIcon}
+            alt = 'search'
+            onClick={() => {}}
+            />
+        </div>
+        <div className='container'>
+        <div className='movie'>
+            <div>
+                <p>{movie1.Year}</p>
+            </div>
+            <div>
+                <img src = {movie1.Poster} alt = {movie1.Title} />
+            </div>
+
+        </div>
+        </div>
+       </div>
+    )
+}
+
+export default App
